64a065a14332160c23ac2425a5f1339bd1d53008
robinNcode <msmrobin518@gmail.com>
2025-08-08 03:26:41 +0600

    refactor: unused variable removed.

--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -5,7 +5,7 @@ export function parse(input: string): any {
     return (banglascript_parser as any).parse(input);
   } catch (err: any) {
     if (err.hash && err.hash.loc) {
-      const { first_line, last_line, first_column, last_column } = err.hash.loc;
+      const { first_line} = err.hash.loc;
       const message = err.message.split('\n').slice(2).join('\n'); // Clean up the error message
       throw new Error(`ভুল, লাইন নংঃ ${first_line} 
  ${message} 
