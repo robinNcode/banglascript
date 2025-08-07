0a553fe8877a0ad6ffd740d5708ab5e5f2ca509e
robinNcode <msmrobin518@gmail.com>
2025-08-08 01:46:50 +0600

    fix: build issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import commonjs from '@rollup/plugin-commonjs';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), commonjs()],
+  plugins: [react()],
 })
