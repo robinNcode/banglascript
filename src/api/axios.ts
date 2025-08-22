ef2bbc80c0de9f5025aa603d2449029690a0fd66
robinNcode <msmrobin518@gmail.com>
2025-08-22 23:41:53 +0600

    update: server development config added.

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
+const appMode = import.meta.env.VITE_APP_MODE;
+console.log(appMode);
+const apiUrl = (appMode == 'development') ? import.meta.env.VITE_LOCAL_API : import.meta.env.VITE_PRODUCTION_API;
+
+console.log(`API URL: ${apiUrl}`);
+
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: apiUrl,
 });
 
 export default instance;
